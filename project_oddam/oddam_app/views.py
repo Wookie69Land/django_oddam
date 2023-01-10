4ddc628ebf3e6184c0b494dddd882147b1b18a76
Wookie69Land <lukaskraj@gmail.com>
2023-01-10 09:57:39 +0100

    Stage-0-2 templates and statics

--- project_oddam/oddam_app/views.py
+++ project_oddam/oddam_app/views.py
@@ -8,3 +8,19 @@ from django.core.paginator import Paginator
 class LandingPageView(View):
     def get(self, request):
         return render(request, "index.html")
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
--- project_oddam/project_oddam/urls.py
+++ project_oddam/project_oddam/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from oddam_app.views import LandingPageView
+from oddam_app.views import LandingPageView, AddDonationView, LoginView, \
+    RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='start'),
+    path('add_donation/', AddDonationView.as_view(), name='donation'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
