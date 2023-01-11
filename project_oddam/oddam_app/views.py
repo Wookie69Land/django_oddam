efb80ba3372a29de1bff1e20c170b7e047270fe3
Wookie69Land <lukaskraj@gmail.com>
2023-01-11 19:41:18 +0100

    Stage-2-4 dynamic institution list (pagination bug)

--- project_oddam/oddam_app/views.py
+++ project_oddam/oddam_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.core.paginator import Paginator
 
-from oddam_app.models import Donation
+from oddam_app.models import Donation, Institution
 
 class LandingPageView(View):
     def get(self, request):
@@ -16,8 +16,25 @@ class LandingPageView(View):
             if donation.institution not in institutions:
                 institutions.append(donation.institution)
         institutions_count = len(institutions)
+
+        foundations = get_list_or_404(Institution, type=1)
+        organizations = get_list_or_404(Institution, type=2)
+        collections = get_list_or_404(Institution, type=3)
+        paginator1 = Paginator(foundations, 3)
+        paginator2 = Paginator(organizations, 3)
+        paginator3 = Paginator(collections, 3)
+        page1 = request.GET.get('page1')
+        page2 = request.GET.get('page2')
+        page3 = request.GET.get('page3')
+        foundations = paginator1.get_page(page1)
+        organizations = paginator2.get_page(page2)
+        collections = paginator3.get_page(page3)
+
         return render(request, "index.html", {'bags': bags,
-                                              'institutions': institutions_count})
+                                              'institutions': institutions_count,
+                                              'foundations': foundations,
+                                              'organizations': organizations,
+                                              'collections': collections})
 
 
 class AddDonationView(View):
