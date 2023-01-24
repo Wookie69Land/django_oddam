563a69dbc4d8f33992ba8039b3f210faf3c81916
Wookie69Land <lukaskraj@gmail.com>
2023-01-24 21:46:05 +0100

    Stage 15-1 admin delete validation

--- project_oddam/oddam_app/models.py
+++ project_oddam/oddam_app/models.py
@@ -5,6 +5,8 @@ from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
 from django.core.exceptions import PermissionDenied
 
+from django_currentuser.middleware import get_current_authenticated_user
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -54,6 +56,6 @@ def delete_user(sender, instance, **kwargs):
     if instance.is_superuser:
         if superusers.count() == 1:
             raise PermissionDenied("Nie możesz skasować ostatniego superusera")
-        if sender == instance:
+        if get_current_authenticated_user() == instance:
             raise PermissionDenied("Nie możesz skasować samego siebie")
 
